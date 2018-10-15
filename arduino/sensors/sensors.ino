5a5b43448d8bceb2c9908d9480dec1748ea91703
eabujayyab <44044957+eabujayyab@users.noreply.github.com>
2018-10-14 22:38:05 -0400

    Update sensors.ino

--- arduino/sensors/sensors.ino
+++ arduino/sensors/sensors.ino
@@ -109,3 +109,45 @@ ISR(TIMER1_COMPA_vect){
     flag1=true;
   }
 }
+
+/****************************************
+Microphone Amplifier
+****************************************/
+ 
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+ 
+void setup() 
+{
+   Serial.begin(9600);
+}
+ 
+ 
+void loop() 
+{
+   unsigned long startMillis= millis();  // Start of sample window
+   unsigned int peakToPeak = 0;   // peak-to-peak level
+ 
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+ 
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(0);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+    
+   Serial.println(peakToPeak);
+}
