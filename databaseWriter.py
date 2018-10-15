355bb940c477bb3da943c40aa6470a9524c4d4a0
Yanushka <ygnan057@uottawa.ca>
2018-10-14 21:49:54 -0400

    Update how serial reads data

--- databaseWriter.py
+++ databaseWriter.py
@@ -6,19 +6,12 @@ import os
 
 class DatabaseWriter():
 
-    def __init__(self):
-
-
-        os.getcwd()
-        config_path = os.getcwd() + "/config/defaultConfig.json"
-
-        with open(config_path, 'r') as f:
-            datastore = json.load(f)
+    def __init__(self, datastore):
 
         self.firebase_url = datastore["firebase-url"]
   
 
-    def run(self, led_reading):  
+    def run(self, measurment):  
 
         #current time and date
         time_hhmmss = time.strftime('%H:%M:%S')
@@ -26,12 +19,14 @@ class DatabaseWriter():
         
         #current location name
         led_location = 'Ottawa'
-        print led_reading + ',' + time_hhmmss + ',' + date_mmddyyyy + ',' + led_location
+
+        print measurment.getTemperature()
+        # print led_reading + ',' + time_hhmmss + ',' + date_mmddyyyy + ',' + led_location
         
-        #insert record
-        data = {'date':date_mmddyyyy,'time':time_hhmmss,'value':led_reading}
-        result = requests.post(self.firebase_url + '/' + led_location + '/led.json', data=json.dumps(data))
+        # #insert record
+        # data = {'date':date_mmddyyyy,'time':time_hhmmss,'value':led_reading}
+        # result = requests.post(self.firebase_url + '/' + led_location + '/led.json', data=json.dumps(data))
         
-        print 'Record inserted. Result Code = ' + str(result.status_code) + ',' + result.text
+        # print 'Record inserted. Result Code = ' + str(result.status_code) + ',' + result.text
      
         
