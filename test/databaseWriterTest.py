20e20f620a9e643bb8dcc5eec74d7d15b72afa57
mahnoorcheema <mchee050@uottawa.ca>
2018-11-28 21:58:53 -0500

    Limits databaseWriters knowledge of config to firestore portion
    The configuration passed to the database writer now only contains
    information relevant to it. Additionally the responsibility of saving to
    the cloud or choosing not to has been migrated to the caller of database
    writer.

--- test/databaseWriterTest.py
+++ test/databaseWriterTest.py
@@ -1,5 +1,4 @@
 import json
-import os
 from random import randint
 
 from databaseWriter import DatabaseWriter
@@ -7,17 +6,17 @@ from databaseWriter import Firestore
 from readSerial import Measurment
 
 
-def test_database_writer(config):
+def test_database_writer(firestore_config):
     print("Testing database writer")
-    database_writer = DatabaseWriter(config)
-    measurement = Measurment(randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 4))
+    database_writer = DatabaseWriter(firestore_config)
+    measurement = Measurment(randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 4), randint(0, 100))
     database_writer.run(measurement)
 
 
-def test_firestore(config):
+def test_firestore(firestore_config):
     print("Testing firestore")
-    firestore = Firestore(config["api-key"], "")
-    firestore.login(config["email"], config["password"])
+    firestore = Firestore(firestore_config["api-key"], "")
+    firestore.login(firestore_config["email"], firestore_config["password"])
     firestore.refresh()
 
 
@@ -25,7 +24,7 @@ if __name__ == '__main__':
     config_path = "../config/defaultConfig.json"
     with open(config_path, 'r') as f:
         config = json.load(f)
-    config["store_database"] = True
-    test_firestore(config)
-    test_database_writer(config)
+    firestore_config = config["firestore"]
+    test_firestore(firestore_config)
+    test_database_writer(firestore_config)
 
