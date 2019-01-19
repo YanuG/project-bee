d4a512589f7d71da6bf9aa53b8f9f377c37ab8cb
Yanushka <ygnan057@uottawa.ca>
2019-01-19 02:49:18 -0500

    Adda parameter to control amount time to write to database

--- readSerial.py
+++ readSerial.py
@@ -7,32 +7,25 @@ import sched, time
 from databaseWriter import DatabaseWriter
 
 class ReadSerial:
-
     def __init__(self):
 
         os.getcwd()
         config_path = os.getcwd() + "/config/defaultConfig.json"
-
         with open(config_path, 'r') as f:
             self.datastore = json.load(f)
-
         # Connect to Serial Port for communication
         self.ser = serial.Serial(self.datastore["arduino-settings"]["port"], self.datastore["arduino-settings"]["baud"], timeout=0)
         # Setup a loop to send values at fixed intervals in seconds
         self.fixed_interval = self.datastore["fixed-interval"]
-
         # Create repository object
         self.save_to_cloud = self.datastore["save_to_cloud"]
         #self.database_writer = DatabaseWriter(self.datastore["firestore"])
         self.start_timer = time.time() 
 
     def run(self):
- 
         while True: 
-
             if self.ser.isOpen():
                 read_serial = self.ser.readline()
-
             if (self.datastore["display"]):
                 print read_serial
             if (len(read_serial) == 27):
@@ -43,18 +36,15 @@ class ReadSerial:
                 frequency = int(read_serial[14:19], 16)
                 bee_count = int(read_serial[20:24], 16)
                 measurement = Measurement(humidity, temperature, frequency, air_quality, bee_count)
-
-            if time.time() - self.start_timer >= 20:
+            if time.time() - self.start_timer >= self.datastore["write-to-database"]:
                 if self.save_to_cloud:
                     self.database_writer.save_measurement(measurement)
                 self.start_timer = time.time()
-
             time.sleep(self.fixed_interval)
                   
 class Measurement:
     
     def __init__(self, humidity, temperature, frequency, air_quality, bee_count):
-        
         self.humidity = humidity
         self.temperature = temperature
         self.frequency = frequency
