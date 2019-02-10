7771b688bae89c64d88cb7ceba81f54fdcb08abb
eabujayyab <44044957+eabujayyab@users.noreply.github.com>
2019-02-10 18:51:08 -0500

    Update sensors.ino

--- arduino/sensors/sensors.ino
+++ arduino/sensors/sensors.ino
@@ -3,6 +3,8 @@
 	#include"AirQuality.h"
 	#include"Arduino.h"
 	#include "arduinoFFT.h"
+	#include "HX711.h"
+
 
 
 	dht DHT; //Object for temp sensor
@@ -17,27 +19,34 @@
 	#define SAMPLES 128             //Must be a power of 2
 	#define SAMPLING_FREQUENCY 9999 //Hz, must be less than 10000 due to ADC
 
-  //Pins
-  const int LIGHT_PIN0 = A3;
-  const int LIGHT_PIN1 = A4;
+  	//Pins
+  	const int LIGHT_PIN0 = A3;
+  	const int LIGHT_PIN1 = A4;
+  	HX711 scale(A1, A0); //*These are already in use*//
+
   
 	//Variables
 	int hum, temp;  //Stores humidity and temperature values
-  int current_quality; //Stores air quality value
-  int freq; //Stores microphone maximum frequency value
+  	int current_quality; //Stores air quality value
+  	int freq; //Stores microphone maximum frequency value
 	int sampling_period_us;
 	long microseconds; 
 	double vReal[SAMPLES]; //Stores real part of microphone frequency
 	double vImag[SAMPLES]; //Stores imaginary part of microphone frequency
-  int numBees; //Stores the number of bees inside the hive (int)
-  int lightThreshold = 10; //could be changed later
-  int curState = -1;
-  int prevState = -1;
-  // states are (' means it is blocked):
-  // both unblocked = 0
-  // outside', inside = 1
-  // outside', inside' = 2
-  // outside, inside' = 3
+  	int numBees; //Stores the number of bees inside the hive (int)
+  	int lightThreshold = 10; //could be changed later
+  	int curState = -1;
+  	int prevState = -1;
+	double read_ADC; 
+	double load; //Store weight value
+	double load_average; //Store average weight value
+
+
+  	// states are (' means it is blocked):
+  	// both unblocked = 0
+ 	// outside', inside = 1
+  	// outside', inside' = 2
+ 	// outside, inside' = 3
 	 
 
 	boolean sensor_interrupt = false; //Interrupt for sensors
@@ -48,7 +57,7 @@
 	
 	void setup(){
   
-    //Set serial baud
+   	//Set serial baud
 		Serial.begin(9600);   
 		cli();//stop interrupts
 	 
@@ -62,27 +71,34 @@
 		TIMSK1 |= (1 << OCIE1A); //Enable timer compare interrupt
 		sei(); //Allow interrupts
 
-    //Initalize airqualitysensor
+   	//Initalize airqualitysensor
 		airqualitysensor.init(14);
 
-    //Initialize Pins for Bee counter
-    pinMode(LIGHT_PIN0, INPUT); 
-    pinMode(LIGHT_PIN1, INPUT);
+    	//Initialize Pins for Bee counter
+    		pinMode(LIGHT_PIN0, INPUT); 
+    		pinMode(LIGHT_PIN1, INPUT);
+		
+	//This value is obtained by calibrating the scale with known weights
+		scale.set_scale(400.f); 
+  	
+	//Reset the scale to 0	
+		scale.tare();		
+
 
 	}
 
 	
 	void loop(){
 
-     if (sensor_interrupt){
-        temperatureSensor();
-        airQualitySensor();
-        sendMessage();
-     }
+     		if (sensor_interrupt){
+        		temperatureSensor();
+        		airQualitySensor();
+        		sendMessage();
+     		}
 	   
-	   microphoneSensor();
-     beeCounter();
-     
+	   	microphoneSensor();
+     		beeCounter();
+		loadSensor();
      
 	}
 
@@ -100,41 +116,41 @@
 		}
 	}
 
-  void changeState(int nextState){
-//    Serial.print("curState: ");
-//    Serial.println(curState);
-//    Serial.print("prevState: ");
-//    Serial.println(prevState);
-    if(nextState != curState){
-      prevState = curState;
-      curState = nextState;
-      if(curState == 0){ //bees have finished entering/exiting
-        if(prevState == 1){
-          --numBees;
-//          Serial.print("numBees: ");
-//          Serial.println(numBees);
-        }else if(prevState == 3){
-          ++numBees;
-//          Serial.print("numBees: ");
-//          Serial.println(numBees);
-        }
-      }
-    }
-  }
+  	void changeState(int nextState){
+		//    Serial.print("curState: ");
+		//    Serial.println(curState);
+		//    Serial.print("prevState: ");
+		//    Serial.println(prevState);
+   		if(nextState != curState){
+      			prevState = curState;
+      			curState = nextState;
+      			if(curState == 0){ //bees have finished entering/exiting
+        			if(prevState == 1){
+          			--numBees;
+//         			Serial.print("numBees: ");
+//          			Serial.println(numBees);
+        			}else if(prevState == 3){
+          				++numBees;
+//          				Serial.print("numBees: ");
+//         				Serial.println(numBees);
+       				}
+      			}
+    		}
+  	}
 
-  void beeCounter(){
-    int outside = analogRead(LIGHT_PIN0);
-    int inside = analogRead(LIGHT_PIN1);
-    if(outside > lightThreshold && inside > lightThreshold){ //both unblocked
-      changeState(0);
-    }else if(outside < lightThreshold && inside > lightThreshold){ //outside is blocked
-      changeState(1);
-    }else if(outside < lightThreshold && inside < lightThreshold){ //both blocked
-      changeState(2);
-    }else if(outside > lightThreshold && inside < lightThreshold){ //inside is blocked 
-      changeState(3); 
-    }
-  }
+  	void beeCounter(){
+    		int outside = analogRead(LIGHT_PIN0);
+    		int inside = analogRead(LIGHT_PIN1);
+    			if(outside > lightThreshold && inside > lightThreshold){ //both unblocked
+   				  changeState(0);
+    			}else if(outside < lightThreshold && inside > lightThreshold){ //outside is blocked
+   				   changeState(1);
+   			}else if(outside < lightThreshold && inside < lightThreshold){ //both blocked
+      				   changeState(2);
+    			}else if(outside > lightThreshold && inside < lightThreshold){ //inside is blocked 
+   				   changeState(3); 
+    			}
+  	}
 
 
 	void temperatureSensor(){
@@ -168,8 +184,8 @@
 	 
 	 
 		/*FFT*/
-		FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD); //
-		FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD); //
+		FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+		FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
 		FFT.ComplexToMagnitude(vReal, vImag, SAMPLES); //Convert the complex number vReal, vImag into a magnitude
 		freq = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY); //Store maximum frequency value (peak) in freq
 	 
@@ -177,24 +193,32 @@
 	}
 
 
-void convertToHex(int value, int precision) {
+	void loadSensor(){
+		
+		  load = scale.get_units();
 
-    char tmp[16];
-    char format[128];
-    sprintf(format, "0x%%.%dX", precision);
-    sprintf(tmp, format, value);
-    Serial.print(tmp);
-    Serial.print(" ");
-  }
+	}
+
+
+	void convertToHex(int value, int precision) {
+	
+    		char tmp[16];
+    		char format[128];
+    		sprintf(format, "0x%%.%dX", precision);
+   		sprintf(tmp, format, value);
+    		Serial.print(tmp);
+    		Serial.print(" ");
+  	}
 
 	
 	void sendMessage(){
 
 		convertToHex(hum , 2);
-    convertToHex(temp, 2);
+    		convertToHex(temp, 2);
 		convertToHex(current_quality, 1);
-    convertToHex(freq, 3);
-    convertToHex(numBees, 2);
+    		convertToHex(freq, 3);
+   		convertToHex(numBees, 2);
+		convertToHex(load, 1);
 
 
 	}
