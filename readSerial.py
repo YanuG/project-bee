3e696bfb1c5308cc8b52dc11786f9bd82820f35d
Yanushka <ygnan057@uottawa.ca>
2019-03-28 22:59:53 -0400

    Add a test for database

--- readSerial.py
+++ readSerial.py
@@ -13,8 +13,10 @@ class ReadSerial:
         config_path = project_root/"config/defaultConfig.json"
         with open(str(config_path), 'r') as f:
             self.config_file = json.load(f)
+        self.database_test = self.config_file["test"]
         # Connect to Serial Port for communication
-        self.ser = serial.Serial(self.config_file["arduino-settings"]["port"], self.config_file["arduino-settings"]["baud"], timeout=0)
+        if (not self.database_test):
+            self.ser = serial.Serial(self.config_file["arduino-settings"]["port"], self.config_file["arduino-settings"]["baud"], timeout=0)
         # Setup a loop to send values at fixed intervals in seconds
         self.fixed_interval = self.config_file["fixed-interval"]
         # Create repository object
@@ -24,19 +26,28 @@ class ReadSerial:
 
     def run(self):
         while True: 
-            if self.ser.isOpen():
-                read_serial = self.ser.readline()
-            if (self.config_file["display"]):
-                print read_serial
-            if (len(read_serial) == 27):
-                # read sensor data 
-                humidity = int(read_serial[0:4], 16)
-                temperature = int(read_serial[5:9], 16)
-                air_quality = int(read_serial[10:13], 16)
-                frequency = int(read_serial[14:19], 16)
-                bee_count = int(read_serial[20:24], 16)
+            if (self.database_test):
+                humidity = 30
+                temperature = 24
+                air_quality = 3
+                frequency = 1000
+                bee_count = 30
                 measurement = Measurement(humidity, temperature, frequency, air_quality, bee_count)
+            else:
+                if self.ser.isOpen():
+                    read_serial = self.ser.readline()
+                if (self.config_file["display"]):
+                    print read_serial
+                if (len(read_serial) == 27):
+                    # read sensor data 
+                    humidity = int(read_serial[0:4], 16)
+                    temperature = int(read_serial[5:9], 16)
+                    air_quality = int(read_serial[10:13], 16)
+                    frequency = int(read_serial[14:19], 16)
+                    bee_count = int(read_serial[20:24], 16)
+                    measurement = Measurement(humidity, temperature, frequency, air_quality, bee_count)
             if time.time() - self.start_timer >= self.config_file["write-to-database"]:
+                print "Writing"
                 if self.save_to_cloud:
                     self.database_writer.save_measurement(measurement)
                 self.start_timer = time.time()
